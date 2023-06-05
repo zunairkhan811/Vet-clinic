126f8ad02a20fe75a5014d1abb9ae2d470cfd878
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-06 01:42:36 +0500

    Add queries.sql code

--- queries.sql
+++ queries.sql
@@ -1,3 +1,13 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM ANIMALS WHERE NAME LIKE '%mon' ;
+SELECT name FROM animals where date_of_birth>='2016-01-01' AND date_of
+_birth<='2019-12-31' ;
+SELECT name from animals where neutered='1' AND escape_attempts<3 ; 
+SELECT date_of_birth from animals where name IN ('Agumon','Pikachu') ;
+SELECT name,escape_attempts from animals where weight_kg>10.5 ;
+SELECT * from animals where neutered=true ;
+SELECT * from animals where name!='Gabumon' ;
+SELECT * from animals where weight_kg>=10.4 AND weight_kg<=17.3 ;
+
+
