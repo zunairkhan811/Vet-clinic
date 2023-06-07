e1f8985537ce6f00544eda22a5230a0adcc2031e
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-08 00:30:20 +0500

    add data in data.sql

--- data.sql
+++ data.sql
@@ -11,4 +11,53 @@ INSERT INTO ANIMALS VALUES (8,'Angemon','2005-06-12',1,true,-45) ;
 INSERT INTO ANIMALS VALUES (9,'Boarmon','2005-06-07',7,true,20.4) ;
 INSERT INTO ANIMALS VALUES (10,'Blossom','1998-10-13',3,true,17) ;
 INSERT INTO ANIMALS VALUES (11,'Ditto','2022-05-14',4,true,22) ;
+Insert into owners values (1,'Sam Smith',34);
+Insert into owners values (2,'Jennifer Orwell',19);
+Insert into owners values (3,'Bob',45);
+Insert into owners values (4,'Melody Pond',77);
+Insert into owners values (5,'Dean Winchester',14);
+Insert into owners values (6,'Jodie Whittaker',38);
+Insert into species values (1,'Pokemon');
+Insert into species values (2,'Digimon');
+
+update animals
+set species_id = (select id from species 
+where name='Digimon')
+where name like ('%mon');
+
+update animals
+set species_id = (select id from species 
+where name='Pokemon')
+where species_id is null;
+
+update animals
+set owner_id = (select id from owners 
+where full_name='Sam Smith')
+where name = 'Agumon';
+
+update animals
+set owner_id = (select id from owners 
+where full_name='Jennifer Orwell')
+where name in ('Gabumon','Pikachu');
+
+update animals
+set owner_id = (select id from owners 
+where full_name='Bob'
+)
+where name in ('Devimon','Plantmon');
+
+update animals
+set owner_id = (select id from owners 
+where full_name='Melody Pond'
+)
+where name in ('Charmander','Squirtle','Blossom');
+
+update animals
+set owner_id = (select id from owners 
+where full_name='Dean Winchester'
+)
+where name in ('Angemon','Boarmon');
+select * from animals ;
+
+
 
